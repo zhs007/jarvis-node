2a6be66215735d3f0ea9f8b01f131ddcb1cfa6e1
zhs007 <sssxueren@gmail.com>
2017-12-28 16:49:33 +0800

    upd

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 "use strict";
 
+const { ProtoMgr } = require('./lib/protomgr');
+const { JarvisServ } = require('./lib/jarvis_serv');
+const { PROTO_JARVISTASK } = require('./lib/protodef');
+const { makeHostAddr } = require('./lib/util');
+
+require('./lib/proto_task');
+
+exports.ProtoMgr = ProtoMgr;
+exports.JarvisServ = JarvisServ;
+
+exports.PROTO_JARVISTASK = PROTO_JARVISTASK;
+
+exports.makeHostAddr = makeHostAddr;
