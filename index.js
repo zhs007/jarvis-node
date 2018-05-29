750656daba769a95ddc5709fbe36c68c6a6ed174
zhs007 <sssxueren@gmail.com>
2018-05-29 17:43:05 +0800

    upd

--- index.js
+++ index.js
@@ -2,17 +2,22 @@
 
 const { ProtoMgr } = require('./lib/protomgr');
 const { JarvisServ } = require('./lib/jarvis_serv');
-const { PROTO_JARVISTASK } = require('./lib/protodef');
-const { makeHostAddr, log, initDailyRotateFileLog, setLogger } = require('./lib/util');
+const { JarvisNode } = require('./lib/jarvis_node');
+const { PROTO_JARVISTASK, PROTO_JARVISNETCORE } = require('./lib/protodef');
+const { makeHostAddr, log, initDailyRotateFileLog, setLogger, genPriKey } = require('./lib/util');
 
 require('./lib/proto_task');
+require('./lib/proto_netcore');
 
 exports.ProtoMgr = ProtoMgr;
 exports.JarvisServ = JarvisServ;
+exports.JarvisNode = JarvisNode;
 
 exports.PROTO_JARVISTASK = PROTO_JARVISTASK;
+exports.PROTO_JARVISNETCORE = PROTO_JARVISNETCORE;
 
 exports.makeHostAddr = makeHostAddr;
 exports.log = log;
 exports.initDailyRotateFileLog = initDailyRotateFileLog;
 exports.setLogger = setLogger;
+exports.genPriKey = genPriKey;
---
+++ samples/node0.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const { JarvisNode } = require('../index');
+
+const cfg = {
+    nodeAddr: '0.0.0.0:7777'
+};
+
+const node = new JarvisNode(cfg);
+node.start();
